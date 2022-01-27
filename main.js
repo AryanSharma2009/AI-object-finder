4d3f2cc563ea3f8ae0d5a5be93e8d0df8b729b83
AryanSharma2009 <78365089+AryanSharma2009@users.noreply.github.com>
2022-01-27 21:10:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+status = "";
+
+function preload(){
+    video = createCapture('video');
+    video.hide();
+}
+
+function setup(){
+   canvas = createCanvas(480, 380);
+   canvas.center(); 
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded!");  
+    status = true;
+}
